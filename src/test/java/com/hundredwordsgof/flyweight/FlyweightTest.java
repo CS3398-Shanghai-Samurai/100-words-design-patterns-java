516d9830658e45af6abc542ee4a1be6869b47723
Immanuel Vattakunnel <35547377+iv63@users.noreply.github.com>
2018-03-28 17:58:14 -0500

    Update FlyweightTest.java

--- src/test/java/com/hundredwordsgof/flyweight/FlyweightTest.java
+++ src/test/java/com/hundredwordsgof/flyweight/FlyweightTest.java
@@ -14,7 +14,7 @@ public class FlyweightTest {
     Flyweight flyweight1 = FlyweightFactory.getFlyweight("key1", "value1");
     Flyweight flyweight2 = FlyweightFactory.getFlyweight("key2", "value2");
     Flyweight flyweight3 = FlyweightFactory.getFlyweight("key1", "value3");
-
+   
     Flyweight unsharedFlyweight1 = FlyweightFactory.getFlyweight("unsharedKey1",
         "value1");
     Flyweight unsharedFlyweight2 = FlyweightFactory.getFlyweight("unsharedKey2",
@@ -25,6 +25,8 @@ public class FlyweightTest {
     assertNotEquals(flyweight1, flyweight2);
     assertEquals(flyweight1, flyweight3);
     assertNotEquals(flyweight2, flyweight3);
+    //the change we made
+    assertEquals(flyweight2, flyweight3);
 
     assertNotEquals(unsharedFlyweight1, unsharedFlyweight2);
     assertNotEquals(unsharedFlyweight1, unsharedFlyweight3);
